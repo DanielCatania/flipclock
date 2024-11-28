f784342a46532ceedd06a50016a1c4dc5fb22025
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2024-11-27 21:46:42 -0300

    adding loading animation

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import ClockService from "./assets/scripts/Clock/index.js";
 import FlipAnimation from "./assets/scripts/FlipAnimation/index.js";
 import ClockTypeManager from "./assets/scripts/ClockTypeManager/index.js";
 
+window.onload = function () {
+  document.querySelector(".loading-overlay").style.display = "none";
+};
+
 const clock = new ClockService();
 const manager = new ClockTypeManager();
 
