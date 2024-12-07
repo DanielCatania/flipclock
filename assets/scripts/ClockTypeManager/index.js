f53b00ad394cd0873cfde57de86041c18c37c026
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2024-12-06 22:29:14 -0300

    preventing error by formatting the page type

--- assets/scripts/ClockTypeManager/index.js
+++ assets/scripts/ClockTypeManager/index.js
@@ -10,15 +10,18 @@ export default class ClockTypeManager {
   }
 
   getType() {
-    return window.location.pathname;
+    const type = window.location.pathname;
+    const formatedType = type.replace(/\//g, "");
+
+    return formatedType;
   }
 
   setClockType(type) {
     switch (type) {
-      case "/timer/":
+      case "timer":
         this.clock = TimerClock;
         break;
-      case "/pomodoro/":
+      case "pomodoro":
         this.clock = PomodoroClock;
         break;
       default:
